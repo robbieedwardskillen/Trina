114def75fa8e25dbcc8ef549db0f076ba0b1a1e2
robbieskillen <rskill01@baker.edu>
2016-09-05 07:28:32 -0400

    kadf

--- js/script.js
+++ js/script.js
@@ -14,11 +14,11 @@ $(function(){
 var dc = {};
 var homeHtml = "snippets/home-snippet.html";
 var allCategoriesUrl =
-  "test";
+  "https://davids-restaurant.herokuapp.com/categories.json";
 var categoriesTitleHtml = "snippets/categories-title-snippet.html";
 var categoryHtml = "snippets/category-snippet.html";
-var menuItemsUrl = //Try it without JSON ***
-  "menu-item-test";
+var menuItemsUrl =
+  "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
 var menuItemsTitleHtml = "snippets/menu-items-title.html";
 var menuItemHtml = "snippets/menu-item.html";
 
