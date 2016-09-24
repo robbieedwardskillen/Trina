0b503f6555a508e4ed17fcd884f4c72e1fc50547
robbieskillen <rskill01@baker.edu>
2016-09-24 09:24:43 -0400

    changed image file names

--- js/script.js
+++ js/script.js
@@ -57,8 +57,8 @@ var menuItemHtml = "snippets/menu-item.html";
 	};
 	dc.loadMenuItems = function(categoryShort){
 		showLoading("#main-content");
-		var randomValues = ["A", "B", "C"];
-		var randomValue = randomValues[Math.floor(Math.random() * randomValues.length)];
+		// var randomValues = ["A", "B", "C"];
+		// var randomValue = randomValues[Math.floor(Math.random() * randomValues.length)];
 		$ajaxUtils.sendGetRequest(menuItemsUrl + categoryShort, buildAndShowMenuItemsHTML);
 	}
 
