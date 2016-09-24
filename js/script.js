9a7cec4512a109745531e5127d95d075cdce7939
robbieskillen <rskill01@baker.edu>
2016-09-24 08:20:48 -0400

    no random category

--- js/script.js
+++ js/script.js
@@ -59,7 +59,7 @@ var menuItemHtml = "snippets/menu-item.html";
 		showLoading("#main-content");
 		var randomValues = ["A", "B", "C"];
 		var randomValue = randomValues[Math.floor(Math.random() * randomValues.length)];
-		$ajaxUtils.sendGetRequest(menuItemsUrl + randomValue, buildAndShowMenuItemsHTML);
+		$ajaxUtils.sendGetRequest(menuItemsUrl + categoryShort, buildAndShowMenuItemsHTML);
 	}
 
 	document.addEventListener("DOMContentLoaded", function(event){
