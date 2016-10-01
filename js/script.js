f85b01b1663b5e0d2e06a9a9c2a4b6c4262c4c14
robbieskillen <rskill01@baker.edu>
2016-10-01 09:09:30 -0400

    kadf

--- js/script.js
+++ js/script.js
@@ -54,8 +54,6 @@ var menuItemHtml = "snippets/menu-item.html";
 	};
 	dc.loadMenuItems = function(categoryShort){
 		showLoading("#main-content");
-		// var randomValues = ["A", "B", "C"];
-		// var randomValue = randomValues[Math.floor(Math.random() * randomValues.length)];
 		$ajaxUtils.sendGetRequest(menuItemsUrl + categoryShort, buildAndShowMenuItemsHTML);
 	}
 
