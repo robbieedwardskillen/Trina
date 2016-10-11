94d69b5de600cf7722d479e18d2a49988f3d01f1
robbieskillen <rskill01@baker.edu>
2016-10-11 13:59:01 -0400

    kadf

--- js/script.js
+++ js/script.js
@@ -8,7 +8,13 @@ $(function(){
 	$("#navbarToggle").click(function (event) {
     	$(event.target).focus();
   	});
-  	
+  	$(".background-image").interactive_bg({
+  		strength: 25,
+   	 	scale: 1.05,
+   		animationSpeed: "100ms",
+   		contain: true,
+   		wrapContent: false
+    });
 });
 
 (function(global){
