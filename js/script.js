7e7d050bb4e8c21b86b30868c424162643e964e6
robbieskillen <rskill01@baker.edu>
2016-10-11 13:55:27 -0400

    kadsf

--- js/script.js
+++ js/script.js
@@ -10,9 +10,12 @@ $(function(){
   	});
   	
 });
-$("#services-tile").mousemove(function(e){
-	var amountMovedY = (e.pageY * -1 / 6);
-	$(this).css('background-position', amountMovedY + 'px ');
+$("#services-tile").interactive_bg({
+	strength: 25, 
+	scale: 1.05,
+	animationSpeed: "100ms",
+	contain: true,
+	wrapContant: false
 });
 (function(global){
 var dc = {};
